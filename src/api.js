2a785bd1ff70d68c3858941332af69807e4850c6
bawlamarwan09-cmyk <bawlamarwan09@gmail.com>
2025-12-04 12:44:57 +0100

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const api = axios.create({
-  baseURL: "http://localhost/gym",
+  baseURL: "https://rachidgym.infinityfreeapp.com",
   timeout: 10000,
 });
 export default api;
